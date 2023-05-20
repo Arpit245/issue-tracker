bee640ec750e6f5314693900b6aeb7e247bea524
ARPIT OJHA <arpitrishu.ojha33@gmail.com>
2023-05-20 11:47:22 +0530

    Second Commit:Route and Controller setup

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const router = express.Router()
+const homeController = require('../controllers/home_controller')
+
+router.get('/',homeController.home)
+
+
+
+
+
+
+
+
+console.log("router running")
+module.exports = router;
